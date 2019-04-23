ee8997635864fb2710d6412022a8c3df997513db
Kitt <stardust427@outlook.com>
2019-04-22 22:24:13 -0600

    Create Averages_and_StdDev.m

---
+++ Diffraction Lab/Averages_and_StdDev.m
@@ -0,0 +1,30 @@
+mR0_04 = mean(sR0_04)
+devR0_04 = std(sR0_04)
+
+mV0_04 = mean(sV0_04)
+devV0_04 = std(sV0_04)
+
+mG0_04 = mean(sG0_04)
+devG0_04 = std(sG0_04)
+
+
+
+mR0_08 = mean(sR0_08)
+devR0_08 = std(sR0_08)
+
+mV0_08 = mean(sV0_08)
+devV0_08 = std(sV0_08)
+
+mG0_08 = mean(sG0_08)
+devG0_08 = std(sG0_08)
+
+
+
+mR0_16 = mean(sR0_16)
+devR0_16 = std(sR0_16)
+
+mV0_16 = mean(sV0_16)
+devV0_16 = std(sV0_16)
+
+mG0_16 = mean(sG0_16)
+devG0_16 = std(sG0_16)
