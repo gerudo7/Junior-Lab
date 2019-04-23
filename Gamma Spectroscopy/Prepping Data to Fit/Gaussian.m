d63a043e18767eda400041e9c717907dccc0895c
Kitt <stardust427@outlook.com>
2019-04-22 22:36:59 -0600

    Create Gaussian.m

---
+++ Gamma Spectroscopy/Prepping Data to Fit/Gaussian.m
@@ -0,0 +1,15 @@
+C = Na(:,1);
+Na22 = Na(:,2);
+Mn54 = Mn(:,2);
+Co60 = Co(:,2);
+Cd109 = Cd(:,2);
+Ba133 = Ba(:,2);
+Unk = Unknown(:,2);
+
+AC = table2array(C);
+ANa22 = table2array(Na22);
+AMn54 = table2array(Mn54);
+ACo60 = table2array(Co60);
+ACd109 = table2array(Cd109);
+ABa133 = table2array(Ba133);
+AUnk = table2array(Unk);
